0582fa0de3dc2f4c8c1b83d8365a6d8956e27c25
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-10-11 00:07:54 -0500

    Add project files.

---
+++ Data/Profile.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Profile
+    {
+  
+        public string DisplayName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        
+        public Profile( string displayname, string email)
+        {
+            
+            DisplayName = displayname;
+            Email = email;
+        }
+
+    }
+}

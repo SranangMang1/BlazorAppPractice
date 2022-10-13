f957e8c5a9ac9dd5fab0414d9e083895698d6146
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-10-13 17:05:53 -0500

    update: Slug from email vs name

--- Data/Profile.cs
+++ Data/Profile.cs
@@ -1,9 +1,13 @@
-﻿namespace BlazorApp1.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
 {
     public class Profile
     {
-  
+
+        [Required, MinLength(2, ErrorMessage ="Please enter a valid name.")]
         public string DisplayName { get; set; } = null!;
+        [Required, EmailAddress(ErrorMessage ="Please enter a valid email address.")]
         public string Email { get; set; } = null!;
         
         public Profile( string displayname, string email)
